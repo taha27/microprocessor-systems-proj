5e0807e3925376da9361063e80a0b2c03408741b
Richardcheung1 <li.cheung@mail.mcgill.ca>
2016-04-04 00:07:15 -0400

    material integration

--- BluetoothLeGatt/Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
+++ BluetoothLeGatt/Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
@@ -37,16 +37,16 @@ public class SampleGattAttributes {
 
     static {
         // Sample Services.
-        attributes.put("0000180d-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
-        attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
+//        attributes.put("0000180d-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
+//        attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
 
         // Temp and Acc Services
         attributes.put(TEMPERATURE_SERVICE, "Temperature Service");
         attributes.put(ACCELEROMETER_SERVICE, "Accelerometer Service");
 
         // Sample Characteristics.
-        attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
-        attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
+//        attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
+//        attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
 
         // Temp and Acc Characteristics
         attributes.put(TEMPERATURE_MEASUREMENT, "Temperature Measurement");
