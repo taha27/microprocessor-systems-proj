9dcce0f0f56ab6d516bcb61be7eab78335bd2d18
Taha Saifuddin <taha.saifuddin@mail.mcgill.ca>
2016-04-03 20:43:16 -0400

    added UUID declarations for temp and acc services and characteristics

--- BluetoothLeGatt/Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
+++ BluetoothLeGatt/Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
@@ -26,13 +26,34 @@ public class SampleGattAttributes {
     public static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb";
     public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
 
+    //COPY_ENV_SENS_SERVICE_UUID and COPY_ACC_SERVICE_UUID in sensor_service.c
+    public static String TEMPERATURE_SERVICE = "42821a40-e477-11e2-82d0-0002a5d5c51b";
+    public static String ACCELEROMETER_SERVICE = "02366e80-cf3a-11e1-9ab4-0002a5d5c51b";
+
+    //COPY_TEMP_CHAR_UUID, COPY_FREE_FALL_UUID and COPY_ACC_UUID respectively in sensor_service.c
+    public static String TEMPERATURE_MEASUREMENT = "a32e5520-e477-11e2-a9e3-0002a5d5c51b";
+    public static String ROLL_ANGLE_MEASUREMENT = "e23e78a0-cf4a-11e1-8ffc-0002a5d5c51b";
+    public static String PITCH_ANGLE_MEASUREMENT = "340a1b80-cf4b-11e1-ac36-0002a5d5c51b";
+
+
     static {
         // Sample Services.
         attributes.put("0000180d-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
         attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
+
+        // Temp and Acc Services
+        attributes.put(TEMPERATURE_SERVICE, "Temperature Service");
+        attributes.put(ACCELEROMETER_SERVICE, "Accelerometer Service");
+
         // Sample Characteristics.
         attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
         attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
+
+        // Temp and Acc Characteristics
+        attributes.put(TEMPERATURE_MEASUREMENT, "Temperature Measurement");
+        attributes.put(ROLL_ANGLE_MEASUREMENT, "Roll Angle Measurement");
+        attributes.put(PITCH_ANGLE_MEASUREMENT, "Pitch Angle Measurement");
+
     }
 
     public static String lookup(String uuid, String defaultName) {
